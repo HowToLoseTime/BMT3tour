c491fab0bde6569c8e2759bb8f9e15035f2ee9e0
roman <chief@astra-olimp.ru>
2025-03-12 09:46:08 +0300

    add painter

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QPainter>
+#include<QPaintEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,7 +16,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    void paintEvent(QPaintEvent *e);
 private:
     Ui::MainWindow *ui;
 };
